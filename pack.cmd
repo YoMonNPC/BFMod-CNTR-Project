02028868f52508720e205a93256c49ac6c2ee635
YoMonNPC <yomoncraft@outlook.com>
2025-07-20 23:40:31 +0800

    更新至新版本

--- pack.cmd
+++ pack.cmd
@@ -3,11 +3,6 @@ chcp 65001
 
 del /q /s "outputs" 
 
-"7z2409-extra\7za.exe" a -sccUTF-8 -scsUTF-8 -tzip -y "outputs\all.zip" @file-lists\\all.txt
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\all.zip" "assets\bf\font\bold-all.json" "assets\bf\font\bold.json"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\all.zip" "pack-all.mcmeta" "pack.mcmeta"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\all.zip" "pack-all.png" "pack.png"
-
 "7z2409-extra\7za.exe" a -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" @file-lists\\schinese.txt
 "7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "assets\bf\font\bold-schinese.json" "assets\bf\font\bold.json"
 "7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "assets\bf\texts\splashes-schinese.txt" "assets\bf\texts\splashes.txt"
@@ -20,7 +15,6 @@ del /q /s "outputs"
 "7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "pack-tchinese.mcmeta" "pack.mcmeta"
 "7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "pack-tchinese.png" "pack.png"
 
-rename "outputs\all.zip" "BFMod-CNTR-Project-All.zip"
 rename "outputs\schinese.zip" "BFMod-CNTR-Project-SC.zip"
 rename "outputs\tchinese.zip" "BFMod-CNTR-Project-TC.zip"
 
