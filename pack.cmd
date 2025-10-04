2b083881d8ceca42b79dc4945462759c9e53e8f0
YoMonNPC <yomoncraft@outlook.com>
2025-10-04 11:24:56 +0800

    更新至新版本

--- pack.cmd
+++ pack.cmd
@@ -3,17 +3,17 @@ chcp 65001
 
 del /q /s "outputs" 
 
-"7z2409-extra\7za.exe" a -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" @file-lists\\schinese.txt
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "assets\bf\font\bold-schinese.json" "assets\bf\font\bold.json"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "assets\bf\texts\splashes-schinese.txt" "assets\bf\texts\splashes.txt"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "pack-schinese.mcmeta" "pack.mcmeta"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "pack-schinese.png" "pack.png"
+"7zip-extra\7za.exe" a -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" @file-lists\\schinese.txt
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "assets\bf\font\bold-schinese.json" "assets\bf\font\bold.json"
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "assets\bf\texts\splashes-schinese.txt" "assets\bf\texts\splashes.txt"
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "pack-schinese.mcmeta" "pack.mcmeta"
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\schinese.zip" "pack-schinese.png" "pack.png"
 
-"7z2409-extra\7za.exe" a -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" @file-lists\\tchinese.txt
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "assets\bf\font\bold-tchinese.json" "assets\bf\font\bold.json"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "assets\bf\texts\splashes-tchinese.txt" "assets\bf\texts\splashes.txt"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "pack-tchinese.mcmeta" "pack.mcmeta"
-"7z2409-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "pack-tchinese.png" "pack.png"
+"7zip-extra\7za.exe" a -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" @file-lists\\tchinese.txt
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "assets\bf\font\bold-tchinese.json" "assets\bf\font\bold.json"
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "assets\bf\texts\splashes-tchinese.txt" "assets\bf\texts\splashes.txt"
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "pack-tchinese.mcmeta" "pack.mcmeta"
+"7zip-extra\7za.exe" rn -sccUTF-8 -scsUTF-8 -tzip -y "outputs\tchinese.zip" "pack-tchinese.png" "pack.png"
 
 rename "outputs\schinese.zip" "BFMod-CNTR-Project-SC.zip"
 rename "outputs\tchinese.zip" "BFMod-CNTR-Project-TC.zip"
